76a413687aa1ea895d8dfc013bfd71cdd0ffcdc3
Ryan Steele <intangiblehawk@gmail.com>
2016-02-01 14:03:17 -0700

    Started work on Tests for Dependency Graph

---
+++ Spreadsheet/DependencyGraphTests/dependencyTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dependencies;
+namespace DependencyGraphTests
+{
+    [TestClass]
+    public class dependencyTests
+    {
+        [TestMethod]
+        public void graphTest1()
+        {
+            DependencyGraph graph = new DependencyGraph();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void graphTest2()
+        {
+
+        }
+    }
+}
